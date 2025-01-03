e0f284bcb41a2c0abb0cb2177a388a115f1d16dd
emerson <emersoneletrotecnico2013@hotmail.com>
2025-01-03 01:11:58 -0300

    admin
    adicionando no administrativo do django

--- travel_packages/apps.py
+++ travel_packages/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class TravelPackagesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'travel_packages'
+    verbose_name = 'Pacote De Viagem'
--- travel_packages/models.py
+++ travel_packages/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import os
 
 class TravelPackage(models.Model):
     name = models.CharField(max_length=100, verbose_name='Nome')
@@ -10,6 +11,28 @@ class TravelPackage(models.Model):
 
     class Meta:
         db_table = 'travel_packages'
+        verbose_name = 'Pacote De Viagem'
+        verbose_name_plural = 'Pacotes De Viagens'
+
+    def save(self, *args, **kwargs):
+        # Verificar se o objeto já existe no banco de dados
+        if self.pk:
+            # Buscar o objeto atual do banco
+            old_image = TravelPackage.objects.get(pk=self.pk).image
+            # Comparar se a imagem foi alterada
+            if old_image and old_image != self.image:
+                # Remover a imagem antiga do sistema de arquivos
+                if os.path.isfile(old_image.path):
+                    os.remove(old_image.path)
+        super().save(*args, **kwargs)  # Salvar o novo registro      
+
+        #subescrever o metodo delete para remover a imagem
+        def delete(self, *args, **kwargs):
+            #remover a imagem associada ao objeto
+            if self.image and os.path.isfile(self.image.path):
+                os.remove(self.image.path)
+            super().delete(*args, **kwargs) #excluir o registro          
 
     def __str__(self):
         return self.name
+
