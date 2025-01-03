c5763fb028dc3334816f2da667cffd5c46fdc827
emerson <emersoneletrotecnico2013@hotmail.com>
2025-01-03 10:42:53 -0300

    views
    views dw black friday

--- travel_packages/views.py
+++ travel_packages/views.py
@@ -4,3 +4,7 @@ from .models import TravelPackage
 def home(request):
     packages = TravelPackage.objects.all()
     return render(request, 'travel_packages/home.html', {'packages': packages})
+
+def black_friday(request):
+    packages = TravelPackage.objects.all().order_by('discounted_price')
+    return render(request, 'travel_packages/black_friday.html', {'packages': packages})
