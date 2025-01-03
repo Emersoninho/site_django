2391e8238ca611ecec140d1431ae2c62907b858f
emerson <emersoneletrotecnico2013@hotmail.com>
2025-01-03 11:26:41 -0300

    views
    views de detalhes

--- travel_packages/views.py
+++ travel_packages/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import TravelPackage
 
 def home(request):
@@ -8,3 +8,7 @@ def home(request):
 def black_friday(request):
     packages = TravelPackage.objects.all().order_by('discounted_price')
     return render(request, 'travel_packages/black_friday.html', {'packages': packages})
+
+def package_detail(request, id):
+    package = get_object_or_404(TravelPackage, id=id)
+    return render(request, 'travel_packages/package_detail.html', {'package': package})
