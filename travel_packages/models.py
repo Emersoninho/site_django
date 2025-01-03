e5ad1305e9e9aa72c5029d0dc1d3e1795b347a02
emerson <emersoneletrotecnico2013@hotmail.com>
2025-01-03 00:16:44 -0300

    models
    criando o modelo

--- travel_packages/models.py
+++ travel_packages/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class TravelPackage(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Nome')
+    original_price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Preço Original')
+    discounted_price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Preço com Desconto')
+    image = models.ImageField(upload_to='travel_packages/', verbose_name='Image')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Data de Criação')
+    update_at = models.DateTimeField(auto_now=True, verbose_name='Data de Edição')
+
+    class Meta:
+        db_table = 'travel_packages'
+
+    def __str__(self):
+        return self.name
